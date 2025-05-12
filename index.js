d7e4ba31812fa2ad8bb1e780eddb2e1fc77153cf
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-05-12 21:50:24 +0600

    mongodb

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const app=express();
-const port=process.env.PORT || 3000;
-
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
@@ -11,11 +11,38 @@ app.use(express.json());
 // user:CRUD
 // pass: R2HKHmVRhQCKNyD0
 
+const uri =
+  "mongodb+srv://CRUD:R2HKHmVRhQCKNyD0@cluster0.nobizgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-app.get("/",(req,res)=>{
-    res.send("simple crud operaton server");
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(port,()=>{
-    console.log(`simple CRUD opertion server running on:${port}`);
-})
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("simple crud operaton server");
+});
+
+app.listen(port, () => {
+  console.log(`simple CRUD opertion server running on:${port}`);
+});
