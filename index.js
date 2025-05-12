27e30b68e99565f168f6cd01430aca3c24d13f08
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-05-13 00:05:50 +0600

    create user

--- index.js
+++ index.js
@@ -25,16 +25,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    const database=client.db('usersdb');
+    const userCollection=database.collection('users')
+
+    app.post("/users", async(req,res)=>{
+        console.log("data in the server",req.body);
+        const newUser=req.body;
+        const result=await userCollection.insertOne(newUser);
+        res.send(result);
+
+
+    })
+    
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
